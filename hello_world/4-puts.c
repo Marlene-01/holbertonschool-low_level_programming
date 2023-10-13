70457a51dc89d8274f124d73eba38f948695b6c6
Marlene-01 <mc.heyliger01@gmail.com>
2023-10-13 14:44:32 -0400

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual puzzle"
+ * main - prints "programming is like building a multilingual puzzle"
  *
- * return: Always 0.
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
