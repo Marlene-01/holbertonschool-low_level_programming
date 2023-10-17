20952eeea796dd1773965bcce2c70bd139d92c59
Marlene-01 <mc.heyliger01@gmail.com>
2023-10-17 10:08:04 -0400

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Numbers
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+	char  m;
+
+	for(m = 0; m <= 10; m++)
+	{
+		printf("%d\n", m);
+	}
+	return (0);
+}
+
