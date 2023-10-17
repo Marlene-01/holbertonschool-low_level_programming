87547affdaac3819e37097d9e8564323f61f96d4
Marlene-01 <mc.heyliger01@gmail.com>
2023-10-17 12:30:03 -0400

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints Smile in the mirror
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	int l;
+
+	for (l > 'z'; l > 'a'; l++)
+	{
+		putchar('0' + l);
+	}
+	{
+		putchar('\n');
+	}
+
+	return (0);
+}
+
