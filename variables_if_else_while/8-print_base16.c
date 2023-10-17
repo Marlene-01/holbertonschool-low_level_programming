124f77008a82a8b431721bf3b1f946141e4a5270
Marlene-01 <mc.heyliger01@gmail.com>
2023-10-17 14:23:46 -0400

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints hexadecimal
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	int h;
+	{
+	putchar("%d", h);
+	}
+	{
+	putchar("%x", h);
+	}
+	{
+	putchar('\n');
+	}
+
+	return (0);
+}
