638d061aae86150ca06bb13a4eacae478863457d
Marlene-01 <mc.heyliger01@gmail.com>
2023-10-17 11:42:54 -0400

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = 0; z <= 10; z++)
+	{
+		putchar(z);
+		putchar('\n');
+	}
+	return (0);
+}
