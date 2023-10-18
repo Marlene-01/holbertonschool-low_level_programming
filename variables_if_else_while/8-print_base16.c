43fc56377c1942fb20aec34661125a9a1ef3cbbc
Marlene-01 <mc.heyliger01@gmail.com>
2023-10-17 20:49:49 -0400

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
- * main - Prints hexadecimal
+ * main - Prints Hexadecimal
  *
  * Return: Always 0.
  *
@@ -12,13 +13,13 @@ int main(void)
 	int h;
 	char d;
 
-	for (h = '0'; h > '9'; h++)
+	for (h = 0; h <= 9; h++)
 	{
-		putchar("%d", h);
+		putchar('0' + h);
 	}
-	for (d = 'a'; d > 'f'; d++)
+	for (d = 'a'; d <= 'f'; d++)
 	{
-		putchar("%c", d);
+		putchar(d);
 	}
 	{
 		putchar('\n');
