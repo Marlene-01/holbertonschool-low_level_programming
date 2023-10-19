3649226bdef505611562952b2e960a8c4b5a81dd
Marlene-01 <mc.heyliger01@gmail.com>
2023-10-19 11:32:55 -0400

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _ islower - check for lowercase character
+ * _islower - check for lowercase character
  * @c: The character to be checked
  * Return: 1 for lowercase character or 0 for anything else
  */
