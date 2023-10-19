9e41655badd755cfc2f4e3f94e4e32ca8314f07d
Marlene-01 <mc.heyliger01@gmail.com>
2023-10-19 10:39:33 -0400

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,11 +12,11 @@ void print_alphabet_x10(void)
 char a;
 int l;
 l = 0;
-while (l < 10);
+while (l < 10)
 {
 for (a = 'a'; a <= 'z'; a++)
 {
-_putchar(a);
+_putchar('0' + a);
 }
 _putchar('\n');
 a++;
