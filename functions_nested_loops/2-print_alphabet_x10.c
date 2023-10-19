e8f71d2f62264f4d3fb112b7c7d707a996f927c4
Marlene-01 <mc.heyliger01@gmail.com>
2023-10-19 10:01:18 -0400

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - alphabet ten times
+ *
+ * Return: void.
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+char a;
+int l;
+l = 0;	
+while (l < 10)
+
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar(a);
+_putchar('\n');
+a++;
+}
+}
